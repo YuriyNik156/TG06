d9b1ff3eb57a3d9740b71253c9cc3c43d3b33379
YuriyNik156 <yuriy.nik156@gmail.com>
2025-08-08 13:37:42 +0300

    final updates

--- lesson/bot.py
+++ lesson/bot.py
@@ -111,42 +111,42 @@ async def send_tips(message: Message):
     await message.answer(tip)
 
 @dp.message(F.text == "Личные финансы")
-async def finances(message: Message, State: FSMContext):
+async def finances(message: Message, state: FSMContext):
     await state.set_state(FinancesForm.category1)
     await message.reply("Введите первую категорию расходов:")
 
 @dp.message(FinancesForm.category1)
-async def finances(message: Message, State: FSMContext):
+async def finances(message: Message, state: FSMContext):
     await state.update_data(category1 = message.text)
     await state.set_state(FinancesForm.expenses1)
     await message.reply("Введите расходы для категории 1:")
 
 @dp.message(FinancesForm.expenses1)
-async def finances(message: Message, State: FSMContext):
+async def finances(message: Message, state: FSMContext):
     await state.update_data(expenses1 = float(message.text))
     await state.set_state(FinancesForm.category2)
     await message.reply("Введите вторую категорию расходов:")
 
 @dp.message(FinancesForm.category2)
-async def finances(message: Message, State: FSMContext):
+async def finances(message: Message, state: FSMContext):
     await state.update_data(category2 = message.text)
     await state.set_state(FinancesForm.expenses2)
     await message.reply("Введите расходы для категории 2:")
 
 @dp.message(FinancesForm.expenses2)
-async def finances(message: Message, State: FSMContext):
+async def finances(message: Message, state: FSMContext):
     await state.update_data(expenses2 = float(message.text))
     await state.set_state(FinancesForm.category3)
     await message.reply("Введите третью категорию расходов:")
 
 @dp.message(FinancesForm.category3)
-async def finances(message: Message, State: FSMContext):
+async def finances(message: Message, state: FSMContext):
     await state.update_data(category3 = message.text)
     await state.set_state(FinancesForm.expenses3)
     await message.reply("Введите расходы для категории 3:")
 
 @dp.message(FinancesForm.expenses3)
-async def finances(message: Message, State: FSMContext):
+async def finances(message: Message, state: FSMContext):
     data = await state.get_data()
     telegram_id = message.from_user.id
     cursor.execute(
